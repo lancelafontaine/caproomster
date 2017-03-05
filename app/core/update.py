992a1407e346afd1606621dacb79629ab2064539
taimoorrana <taimoorrana1@gmail.com>
2017-03-04 20:05:32 -0500

    refactored update.py

--- app/core/update.py
+++ app/core/update.py
@@ -1,41 +1,45 @@
-from app.TDG import WaitingTDG
-from app.mapper import ReservationMapper
+from mapper import ReservationMapper
 from reservation import Reservation
-from app.mapper import WaitingMapper
+from mapper import WaitingMapper
+from waiting import Waiting
 
 
 # waitingList: list containing (waitingId, room, reservee, description, timeslotid, starttime, endtime) entries
 # availabilityList: availabilities for that room (array of strings)
-def updateWaiting(roomId, date, availabilityList):
+def updateWaiting(room_id, date, availability_list):
     i = 0
-    print(availabilityList)
-    waitingList = WaitingMapper.findRoomOnDate(roomId, date)
-    if waitingList is None:
+    waiting_list = WaitingMapper.findRoomOnDate(room_id, date)
+    if waiting_list is None:
         return
     else:
-        for w in waitingList:
-            for a in availabilityList:
-                if a == "Available" and availabilityList[i + 1] == "unavailable" and (i + 10) != 21:
-                    print("first if")
-                    if w[5] == (i + 9) and w[6] == (i + 9):
-                        r = Reservation(w[1], w[2], w[4], w[3])
-                        ReservationMapper.save(r)
-                        WaitingMapper.delete(w)
+        for waiting_with_start_and_end_time in waiting_list:
+
+            start_time = waiting_with_start_and_end_time[5];
+            end_time = waiting_with_start_and_end_time[6];
+            waiting = Waiting(waiting_with_start_and_end_time[1], waiting_with_start_and_end_time[2],
+                              waiting_with_start_and_end_time[4], waiting_with_start_and_end_time[3],
+                              waiting_with_start_and_end_time[0])
+
+            for time_availability in availability_list:
+                if time_availability == "Available" and availability_list[i + 1] == "unavailable" and (i + 10) != 21:
+                    if start_time == (i + 9) and end_time == (i + 9):
+                        create_reservation(waiting.room, waiting.user, waiting.time, waiting.description,
+                                           waiting_with_start_and_end_time)
                         return
-                elif a == "Available" and availabilityList[i + 1] == "Available" and (i + 10) != 20:
-                    print("first elif")
-                    if w[5] == (i + 9) and w[6] == (i + 10):
-                        print("first elif if")
-                        r = Reservation(w[1], w[2], w[4], w[3])
-                        ReservationMapper.save(r)
-                        WaitingMapper.delete(w)
+                elif time_availability == "Available" and availability_list[i + 1] == "Available" and (i + 10) != 20:
+                    if start_time == (i + 9) and end_time == (i + 10):
+                        create_reservation(waiting.room, waiting.user, waiting.time, waiting.description,
+                                           waiting_with_start_and_end_time)
                         return
-                elif (i + 10) == 21 and a == "Available":
-                    print("second elif")
-                    if w[5] == 21 and w[6] == 21:
-                        print("second elif if")
-                        r = Reservation(w[1], w[2], w[4], w[3])
-                        ReservationMapper.save(r)
-                        WaitingMapper.delete(w)
+                elif (i + 10) == 21 and time_availability == "Available":
+                    if start_time == 21 and end_time == 21:
+                        create_reservation(waiting.room, waiting.user, waiting.time, waiting.description,
+                                           waiting_with_start_and_end_time)
                         return
             i = i + 1
+
+
+def create_reservation(room, holder, time, description, waiting_with_start_and_end_time):
+    r = Reservation(room, holder, time, description)
+    ReservationMapper.save(r)
+    WaitingMapper.delete(waiting_with_start_and_end_time)
