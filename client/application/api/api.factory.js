43133e8affdf0d3c091467d1f4db153f7536e48c
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-03-22 23:08:37 -0400

    create api factory and service module

---
+++ client/application/api/api.factory.js
@@ -0,0 +1,47 @@
+(function(angular) {
+
+    'use strict';
+
+    angular.module('caproomster.api').factory('caproomster.api.ApiFactory', ApiFactory);
+
+    ApiFactory.$inject = ['$resource', 'caproomster.AppConstant'];
+
+    function ApiFactory($resource, AppConstant) {
+
+        var baseUrl = AppConstant.server_base_url;
+
+        return {
+            a: a,
+            b: b
+        };
+
+        // Register
+
+        // Update
+
+        function a() {
+            var url = '/a';
+            return $resource(baseUrl + url, {}, {
+                post: {
+                    method: 'POST',
+                    withCredentials: true
+                }
+            });
+        }
+
+        // get user skill profile
+
+        function b() {
+            var url = '/b';
+            return $resource(baseUrl + url, {
+                email: '@email'
+            }, {
+                get: {
+                  method: 'GET'
+                }
+            });
+        }
+
+    }
+
+})(angular);
