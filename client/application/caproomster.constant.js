a0af46ea6ab1f9d5877c5a8de515a3c120216219
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2017-03-22 23:11:00 -0400

    create constant file for environment variables

---
+++ client/application/caproomster.constant.js
@@ -0,0 +1,11 @@
+(function(angular) {
+
+  'use strict';
+
+  angular.module('caproomster').constant('caproomster.AppConstant', {
+
+    server_base_url: 'http://127.0.0.1:5000',
+
+  });
+
+})(angular);
