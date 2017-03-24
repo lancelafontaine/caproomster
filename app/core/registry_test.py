5aafbdb8161641fa5bc7880df2d79c1a8ad691d7
taimoorrana <taimoorrana1@gmail.com>
2017-03-23 20:49:10 -0400

    this test has been moved to reservationbook_test

--- app/core/registry_test.py
+++ app/core/registry_test.py
@@ -1,13 +1,6 @@
 import pytest
 from app.core.registry import Registry
 from app.core.reservationbook import ReservationBook
-from app.core.room import Room
-from app.core.timeslot import Timeslot
-from app.core.user import User
-from app.mapper import ReservationMapper
-from app.mapper import TimeslotMapper
-from app.mapper import UserMapper
-
 
 def test_valid_registry_init():
     reservationList = []
@@ -28,26 +21,3 @@ def test_invalid_registry_init():
 
     with pytest.raises(AttributeError) as e:
         registry.fakeAttributeDoesntExist
-
-def test_reservation_repeat():
-
-    # test data
-    start_time = 9
-    end_time = 10
-    room_id = 2
-    block = 0
-    date = '2016-04-20'
-    user_id = 1
-    processed_description = ""
-    user = User(user_id, "", "")
-    room = Room(room_id, False)
-    time_slot = Timeslot(start_time, end_time, date, block, user_id)
-    repeat_amount = 2
-
-    # Execute
-    ReservationMapper.makeNewReservation(room, user, time_slot, processed_description, repeat_amount)
-
-    # Verify
-    assert len(ReservationMapper.find_time_slot_ids(user_id)) == repeat_amount + 1
-    assert len(TimeslotMapper.find_all_timeslots_for_user(user_id)) == repeat_amount + 1
-
