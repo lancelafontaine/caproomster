d8c63e3a33d9c669bf08f36cffa33005f9758fcb
MonsieurPetion <lenzpetion@gmail.com>
2017-04-01 14:28:56 -0400

    git ignore fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 
 # create app
 app = Flask(__name__)
 # enable cors
-#CORS(app, origins='http://localhost:8080', supports_credentials=True)
+CORS(app, origins='http://localhost:8080', supports_credentials=True)
 # connect to config.py
 app.config.from_object('config')
 # secret key
