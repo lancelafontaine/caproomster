7e3427c480ee7ff60766dcfd26b9a15efd37a281
Adrianna Diaz <adrianna.dev@gmail.com>
2017-04-01 19:01:00 -0400

    Fixed rest of tests. Will start adding a couple of tests to reservation book to increase code coverage  for the new methods I added.

--- app/core/equipment_test.py
+++ app/core/equipment_test.py
@@ -2,17 +2,17 @@ import pytest
 from app.core.equipment import Equipment
 
 def test_equipment_with_no_arguments_is_zero_length():
-	equipment = Equipment()
+	equipment = Equipment("equipmentID_uybino")
 	assert 0 == len(equipment)
 
 def test_equipment_getting_number_of_equipment_needed():
-	equipment7 = Equipment(laptops=2,projectors=1,whiteboards=4)
+	equipment7 = Equipment("equipmentID_ibiubi",laptops=2,projectors=1,whiteboards=4)
 	assert 7 == len(equipment7)
 
-	equipment1 = Equipment(0,1,0)
+	equipment1 = Equipment("equipmentID",0,1,0)
 	assert 1 == len(equipment1)
 
-	equipment4 = Equipment(1,2,1)
+	equipment4 = Equipment("equipmentID_12313", 1,2,1)
 	assert 4 == len(equipment4)
 
 
